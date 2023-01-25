c6604e9577ac94de323106d2ac58334c98fce05e
paulolopest <paulotarso-lopes@hotmail.com>
2023-01-25 16:11:31 -0300

    Adicionando component de titulo

--- Estudo/react/componente-funcional/components/src/components/Home.js
+++ Estudo/react/componente-funcional/components/src/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Title from './Title'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Title name="Home"/>
   )
 }
 
---
+++ Estudo/react/componente-funcional/components/src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = ({name}) => {
+  return (
+    <h1 style={{color: "green"}}>{name}</h1>
+  )
+}
+
+export default Title
